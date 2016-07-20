4d662192312e5f8f15c5a41cdda2f24f1ab2e478
Matthew G. Switlik <switlik@admnet.oakland.edu>
2016-07-20 16:14:57 -0400

    IR remote control & bug fixes

--- LEDLetterController.ino
+++ LEDLetterController.ino
@@ -8,6 +8,14 @@
   written 2016-07-19
   by Matthew G. Switlik
  */
+#include <IRLib.h>
+
+int RECV_PIN = 14; // A0
+
+IRrecv My_Receiver(RECV_PIN);
+IRdecode My_Decoder;
+IRdecodeHash My_Hash_Decoder;
+ 
 #define MODE_OFF 0                  // All letters off
 #define MODE_ALL_ON 1               // All letters on
 #define MODE_BLINK_EACH 2           // Blink each letter on/off in succesion.
@@ -37,6 +45,10 @@ byte mode = MODE_OFF;
 
 
 void setup() {
+  My_Receiver.enableIRIn(); // Start the receiver
+  Serial.begin(9600);
+  delay(2000);while(!Serial);//delay for Leonardo
+  
   // Initialize digital output pins for each letter and signal.
   for (byte ltr = 0; ltr<LETTERS; ltr++) {
     for (byte sig = 0; sig<SIGNALS; sig++) {
@@ -48,6 +60,55 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  // IR Receiver code
+  if (My_Receiver.GetResults(&My_Decoder)) {//Puts results in My_Decoder
+    My_Hash_Decoder.copyBuf(&My_Decoder);//copy the results to the hash decoder
+    My_Decoder.decode();
+    My_Hash_Decoder.decode();
+    Serial.println(My_Hash_Decoder.hash); // Do something interesting with this value
+    
+    //Power Button
+    if (My_Hash_Decoder.hash == 403429887) {
+      Serial.println("Power Button");
+      if (mode == MODE_OFF) {
+        mode = MODE_ALL_ON;
+      } else {
+        mode = MODE_OFF;
+      }
+
+    // 0 Button  
+    } else if (My_Hash_Decoder.hash == 3509629087) {
+      Serial.println("0 Button");
+      mode = MODE_OFF;
+
+    // 1 Button  
+    } else if (My_Hash_Decoder.hash == 719194275) {
+      Serial.println("1 Button");
+      mode = MODE_ALL_ON;
+
+    // 2 Button
+    } else if (My_Hash_Decoder.hash == 2137078331) {
+      Serial.println("2 Button");
+      mode = MODE_BLINK_EACH;
+
+    // 3 Button
+    } else if (My_Hash_Decoder.hash == 440841023) {
+      Serial.println("3 Button");
+      mode = MODE_BLINK_EACH_THEN_ALL;
+
+    // 4 Button
+    } else if (My_Hash_Decoder.hash == 2671644771) {
+      Serial.println("4 Button");
+      mode = MODE_CRAWL;
+
+    // 5 Button
+    } else if (My_Hash_Decoder.hash == 800219743) {
+      Serial.println("5 Button");
+      mode = MODE_WAVE;
+    }
+
+    My_Receiver.resume(); 
+  }
   
   if (mode == MODE_OFF) {
     // All letters off
@@ -160,34 +221,35 @@ void loop() {
 
 void setleds(byte steps, byte patterns[][4], int durations[]) 
 {
-    // For each step.
-    for (byte s=0; s<steps; s++) {
-      // For each Letter
-      for (byte ltr=0; ltr<LETTERS; ltr++) {
-        // Get the patterns bitwise representation.
-        byte pat = patterns[s][ltr];
-
-        // Bitwise AND the pattern with the A signal bit. On if matches else off.
-        if (pat & A == A) {
-          digitalWrite(lsp[ltr][0], HIGH);
-        } else {
-          digitalWrite(lsp[ltr][0], LOW);
-        }
-
-        // Bitwise AND the pattern with the B signal bit. On if matches else off.
-        if (pat & B == B) {
-          digitalWrite(lsp[ltr][1], HIGH);
-        } else {
-          digitalWrite(lsp[ltr][1], LOW);
-        }
-
-        // Bitwise AND the pattern with the C signal bit. On if matches else off.
-        if (pat & C == C) {
-          digitalWrite(lsp[ltr][2], HIGH);
-        } else {
-          digitalWrite(lsp[ltr][2], LOW);
-        }
+  // For each step.
+  for (byte s=0; s<steps; s++) {
+    // For each Letter
+    for (byte ltr=0; ltr<LETTERS; ltr++) {
+      // Get the patterns bitwise representation.
+      byte pat = patterns[s][ltr];
+      
+      // Bitwise AND the pattern with the A signal bit. On if matches else off.
+      if ((pat & A) == A) {
+        digitalWrite(lsp[ltr][0], HIGH);
+      } else {
+        digitalWrite(lsp[ltr][0], LOW);
       }
-      delay(durations[s]);
+
+      // Bitwise AND the pattern with the B signal bit. On if matches else off.
+      if ((pat & B) == B) {
+        digitalWrite(lsp[ltr][1], HIGH);
+      } else {
+        digitalWrite(lsp[ltr][1], LOW);
+      }
+
+      // Bitwise AND the pattern with the C signal bit. On if matches else off.
+      if ((pat & C) == C) {
+        digitalWrite(lsp[ltr][2], HIGH);
+      } else {
+        digitalWrite(lsp[ltr][2], LOW);
+      }
     }
+    Serial.println(".");
+    delay(durations[s]);
+  }
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # LEDLetterController
 Turn LEDs mounted in large letters on and off based on predefined modes. Each letter has an A, B & C control   signal for the even outer LED, the odd outer LEDs, and the center LEDs.
+
+
+
+pwr:	0x403429887
+0:		0x3509629087
+1:		0x719194275
+2:		0x2137078331
+3:		0x440841023
+4:		0x2671644771
+5:		0x800219743
